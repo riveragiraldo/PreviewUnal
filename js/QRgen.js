d000e1ddf3c2ccdc2b054e37d9f3c99582385331
Andrés Rivera <47341112+riveragiraldo@users.noreply.github.com>
2023-03-10 12:36:44 -0500

    Add files via upload

--- js/QRgen.js
+++ js/QRgen.js
@@ -1,4 +1,19 @@
 const contenedorQR = document.getElementById('contenedorQR');
+const formulario = document.getElementById('formulario');
+
+const QR = new QRCode (contenedorQR);
+QR.makeCode(formulario.link.value);
 
 
-new QRCode(contenedorQR,'https://d1xb30i372mnxf.cloudfront.net');
+
+const contenedorQR_2 = document.getElementById('contenedorQR_2');
+const formulario_2 = document.getElementById('formulario_2');
+
+const QR_2 = new QRCode (contenedorQR_2);
+QR_2.makeCode(formulario_2.link_2.value);
+
+
+
+
+
+
